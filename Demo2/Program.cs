eaa4f90b32bbf28bdf013dddb83cf3d9a543cd43
Jaydan-Miranda <jmmiranda@amarillocollege.com>
2025-01-21 13:50:35 -0600

    Add project files.

---
+++ Demo2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter an even or odd integer. ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+
+            //is the end of the code
+            Console.ReadLine();
+        }
+    }
+}
